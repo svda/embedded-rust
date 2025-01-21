020e3ddb6f8f1f8f064f22dae08b8bb15d32525b
Sander van den Akker <sander@sandervandenakker.nl>
2025-01-21 21:36:32 +0100

    Switch to different ens160 driver

--- src/tasks/air_quality.rs
+++ src/tasks/air_quality.rs
@@ -4,11 +4,13 @@ use defmt::*;
 use embassy_executor::Spawner;
 use embassy_time::Timer;
 use embassy_rp::i2c::{Config, I2c};
-use ens160_aq::data::{AirQualityIndex, Measurements};
-use ens160_aq::Ens160;
-use heapless::String;
+use ens160::{AirQualityIndex, Ens160};
 
 use crate::system::resources::{AirQualityResources, Irqs};
+
+fn trimhex(s: &str) -> &str {
+    s.strip_prefix("0x").unwrap_or(s.strip_prefix("0X").unwrap_or(s))
+}
 
 #[embassy_executor::task]
 pub async fn air_quality(_spawner: Spawner, r: AirQualityResources) -> ! {
@@ -17,7 +19,7 @@ pub async fn air_quality(_spawner: Spawner, r: AirQualityResources) -> ! {
     let mut config = Config::default();
     config.frequency = 400_000;
 
-    let i2c0 = I2c::new_async(
+    let i2c_bus = I2c::new_async(
         r.i2c,
         r.scl,
         r.sda,
@@ -25,40 +27,31 @@ pub async fn air_quality(_spawner: Spawner, r: AirQualityResources) -> ! {
        config,
     );
 
-    let mut ens160 = Ens160::new_secondary_address(i2c0, embassy_time::Delay);
+    let mut ens160 = Ens160::new(i2c_bus, 0x53);
 
-    ens160.initialize().await.unwrap();
+    ens160.reset().unwrap();
+    Timer::after_millis(1000).await;
+    ens160.operational().unwrap();
 
     loop {
-        if let Ok(status) = ens160.get_status().await {
-            if status.new_data_ready() {  // read all measurements
-                let measurements: Measurements = ens160.get_measurements().await.unwrap();
+        if let Ok(status) = ens160.status() {
+            if status.data_is_ready() {
+                let temp_hum = ens160.temp_and_hum().unwrap();
+                info!("Temperature: {}", temp_hum.0/100);
+                info!("Humidity: {}", temp_hum.1/100);
+                let tvoc = ens160.tvoc().unwrap();
+                info!("TVOC: {}", tvoc);
+                let eco2 = ens160.eco2().unwrap().to_le();
+                info!("CO2: {}", eco2);
 
-                info!("CO2: {}", measurements.co2eq_ppm.get_value());
-                info!("TVOC: {}", measurements.tvoc_ppb);
-                // info!("Quality: {}", measurements.air_quality_index);
-                info!("Ethanol: {}", measurements.etoh);
-                info!("RR: {}", measurements.raw_resistance);
+                // from eCO2
+                //let air_quality_index = AirQualityIndex::try_from(eco2).unwrap();
+                // directly
+                //let air_quality_index = ens160.air_quality_index().unwrap();
+                //info!("Air quality: {}", air_quality_index);
             }
-            // if status.new_group_data_ready() {  // useful to see raw data values
-            //     let group_data: [u8; 8] = ens160.get_group_data().await.unwrap();
-            //     info!(
-            //         "group data = {:#04x} {:#04x} {:#04x} {:#04x} {:#04x} {:#04x} {:#04x} {:#04x}",
-            //         group_data[0].to,
-            //         group_data[1],
-            //         group_data[2],
-            //         group_data[3],
-            //         group_data[4],
-            //         group_data[5],
-            //         group_data[6],
-            //         group_data[7]
-            //     );
-            // }
-            else {
-                info!("no new data ready");
-            }  
         }
-
         Timer::after_millis(5000).await;
     }
+    
 }
